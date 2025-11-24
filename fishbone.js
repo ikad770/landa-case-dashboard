a357b72c23063b0ba1192f824f3010c722edcc02
ikad770 <ikad770@gmail.com>
2025-11-24 23:46:04 +0200

    Update fishbone.js

--- fishbone.js
+++ fishbone.js
@@ -1,4 +1,7 @@
-// fishbone.js – placeholder
+// fishbone.js – placeholder for future deep fishbone visualizations.
+// Currently the RCA logic is handled in rca.js with data from rca-data.js.
+
 (function(){
-  // Future fishbone extensions will be added here
+  // In the future this can render an actual fishbone diagram view
+  // based on the RCA_DATA structure and user selections.
 })();
