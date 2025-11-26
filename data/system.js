2c0a7027649d530c5264d211fd0768625bf1bbac
ikad770 <ikad770@gmail.com>
2025-11-26 17:08:09 +0200

    Update system.js

--- data/system.js
+++ data/system.js
@@ -1,68 +1,58 @@
-// data/systems.js
-// Hierarchy for System → SubSystem → HW/SW/EC → Area
+/* ---------------------------------------------------
+   Landa Quantum – System Structure (V20)
+--------------------------------------------------- */
 
-window.SYSTEM_HIER = {
-  "BSS": {
-    subSystems: [
-      "LIB","BCU","BCD&BTD Motors","BACS","Fly-Off","Air Knife",
-      "Hot Plate","Dancer","BCLS","Vacuum Box"
-    ],
-    categories: {
-      HW: ["Rollers","Sensors","Motors","Brackets","Gaskets","Spare Parts","Pipes","Encoders","Valves"],
-      SW: ["LIA","RTC","OPC","Parameters","Inverters Version"],
-      EC: ["Inverters","Cables","SSR","CB's","FEC's"]
-    },
-    areas: [
-      "Feeder infeed","Feeder outfeed","Delivery","Print Engine"
-    ]
+window.SYSTEMS = {
+  "Feeder": {
+    subsystems: {
+      "Loader": ["Pallet", "Elevator", "Side Guides"],
+      "Vacuum": ["Fans", "Nozzles", "Filters"],
+      "Sensors": ["Sheet Sensor", "Jam Sensor", "Skew Sensor"]
+    }
   },
 
   "Print Engine": {
-    subSystems: [
-      "Ink Delivery","Imaging","Registration","Blanket","Developer"
-    ],
-    categories: {
-      HW: ["Heads","Pipes","Sensors","Motors","Actuators"],
-      SW: ["Algorithms","Parameters","Timing","Color Profiles"],
-      EC: ["PSU","Cables","Relays","I/O cards"]
-    },
-    areas: [
-      "Ink Cabinet","Blanket Drum","Registration Zone"
-    ]
+    subsystems: {
+      "Imaging": ["Drum", "Laser", "Registration"],
+      "Ink Delivery": ["Pipes", "Pumps", "Nano Ink Filters"],
+      "Dryer": ["IR Lamps", "Hot Air", "Cooling Fans"]
+    }
   },
 
-  "ICS": {
-    subSystems: ["ICS SW","ICS HW"],
-    categories: {
-      HW: ["Cameras","Lights","Brackets"],
-      SW: ["Software","Recipes"],
-      EC: ["Controllers","Cabling"]
-    },
-    areas: ["Inspection Bridge","ICS Cabinet"]
+  "Coater": {
+    subsystems: {
+      "Varnish": ["Pipes", "Pump", "Tank"],
+      "Anilox": ["Roller", "Doctor Blade"],
+      "UV": ["UV Lamp", "Power Supply"]
+    }
   },
 
   "IRD": {
-    subSystems: ["IRD Dryer"],
-    categories: {
-      HW: ["Emitters","Sensors","Frames"],
-      SW: ["Temperature Control","Profiles"],
-      EC: ["Power modules","Cables"]
-    },
-    areas: ["IRD Unit","Exhaust"]
+    subsystems: {
+      "Mass Balance": ["Flow", "Pressure", "Temperature"],
+      "Pipes": ["Inlet", "Outlet"],
+      "Sensors": ["Temp Sensor", "Level Sensor"]
+    }
   },
 
   "STS": {
-    subSystems: ["Powder","Vacuum"],
-    categories: {
-      HW: ["Powder Bar","Hoses"],
-      SW: ["Control logic"],
-      EC: ["Drives","IO"]
-    },
-    areas: ["Coater","Delivery"]
+    subsystems: {
+      "Powder": ["Pump", "Distributor"],
+      "Vacuum": ["Filters", "Blower"]
+    }
+  },
+
+  "ICS / BCU": {
+    subsystems: {
+      "ICS Software": ["UI", "Database", "Runtime"],
+      "ICS Hardware": ["Controllers", "IO Modules"],
+      "BCU": ["Power", "Drives"]
+    }
+  },
+
+  "Other": {
+    subsystems: {
+      "General": ["N/A"]
+    }
   }
 };
-
-// Pre-build System list for dropdown
-window.SYSTEM_OPTIONS = Object.keys(window.SYSTEM_HIER).map(s => ({
-  value: s, label: s
-}));
